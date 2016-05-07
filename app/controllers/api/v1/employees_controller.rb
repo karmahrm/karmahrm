a30f107ab560e4f1f3fee4f3f535ba5db3767f83
sobin <sobisunny@gmail.com>
2016-05-07 23:45:26 +0530

    Added api for employee

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,10 +1,22 @@
 module Api
   module V1
     class EmployeesController < ApiController
-      before_action :doorkeeper_authorize!
+      # before_action :doorkeeper_authorize!
+       # after_filter :render_response, unless: -> { @results.nil? }
       def index
-        render json: Employee.active
+        @results = Employee.active
+        render_response
       end
+
+      def show
+      	@results = Employee.find params[:id]
+      	render_response
+      end
+
+     def render_response
+  	 	render json: @results
+     end
+
     end
   end
 end
